5d7b2a7e6d005bd9a99f47541f1cdc19f6bf485d
jjxrry <114961129+jjxrry@users.noreply.github.com>
2023-02-09 16:31:24 -0800

    partial mongoDB practice

---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1) 
+}
+
+// const password = process.argv[2]
+
+// console.log(process);
+
+const url = `mongodb+srv://jgao:rand@cluster0.hjr46ja.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+        content: String,
+        important: Boolean,
+    })
+    
+const Note = mongoose.model('Note', noteSchema)
+
+Note.find({/*can add filters/restrictions here EXAMPLE: ----------important: true -------*/}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+    })
+     
+// const note = new Note({
+//     content: `HTML is Easy`,
+//     important: true,
+// })
+
+// note.save().then(result => {
+//     console.log(`note saved!`)
+//     mongoose.connection.close()
+// })
